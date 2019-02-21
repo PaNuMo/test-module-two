89f63f950835bf34fd0b43e3ddf1ce6e33b65e5b
Pablo Nunez <pablo@catalystdevs.com>
2019-02-21 16:29:01 +0100

    Initial commit

---
+++ src/main/java/com/catalyst/module/two/constants/ModuleTwoPortletKeys.java
@@ -0,0 +1,10 @@
+package com.catalyst.module.two.constants;
+
+/**
+ * @author pnunez
+ */
+public class ModuleTwoPortletKeys {
+
+	public static final String ModuleTwo = "moduletwo";
+
+}
---
+++ src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="moduletwo.caption"/></b>
+</p>
